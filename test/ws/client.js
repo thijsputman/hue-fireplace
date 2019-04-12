c905f0495ec9bfe36e34bf6da646ec08aab0427d
Thijs Putman <thijs@sgraastra.net>
2019-04-12 15:20:16 +0200

    Add WebSocket PoC; show effect in a browser window
    Simple proof-of-concept to test whether it is possible (answer: Yes ;)
    to use WebSockets to achieve a similar effect as the Hue Entertainment
    API, but then in a browser window (for ease of further development).

---
+++ test/ws/client.js
@@ -0,0 +1,21 @@
+var exampleSocket = new WebSocket("ws://localhost:8099");
+
+document.body.addEventListener("keydown", e => {
+    if (e.key === "Escape") {
+        exampleSocket.close();
+    }
+});
+
+var reader = new FileReader();
+reader.addEventListener("loadend", function() {
+    const colours = new Uint8Array(reader.result);
+
+    const frame = document.getElementById("colourFrame");
+
+    frame.style.backgroundColor =
+        "rgb(" + colours[0] + "," + colours[1] + "," + colours[2] + ")";
+});
+
+exampleSocket.onmessage = event => {
+    reader.readAsArrayBuffer(event.data);
+};
---
+++ ws.ts
@@ -0,0 +1,44 @@
+import { Server } from "ws";
+import { Support } from "./lib/Support";
+
+const wsServer = new Server({ port: 8099 });
+
+process.on("SIGINT", () => {
+    (async () => {
+        await wsServer.close();
+        console.log("closed");
+
+        process.exit();
+    })();
+});
+
+wsServer.on("connection", function connection(ws) {
+    console.log("connected!");
+
+    (async () => {
+        while (true) {
+            // Message-rate 12.5 Hz
+
+            await Support.delay(80);
+
+            const data = [
+                Math.round(Math.random() * 256),
+                Math.round(Math.random() * 256),
+                Math.round(Math.random() * 256),
+            ];
+            console.log(data);
+
+            try {
+                ws.send(Buffer.from(data));
+            } catch (error) {
+                if (ws.readyState >= 2) {
+                    console.log("closed");
+                } else {
+                    console.log(error);
+                }
+
+                return;
+            }
+        }
+    })();
+});
